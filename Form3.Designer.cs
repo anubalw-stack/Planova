2467e3ff03f298675af14b4efafd5d08577ebda2
Caden-Bleu <clalqueza@gmail.com>
2026-08-19 23:55:34 +1200

    Caden: Event Details template layout and design done

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -34,8 +34,15 @@
             this.PlanovaName = new System.Windows.Forms.Label();
             this.PlanovaLogo = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBoxEvent = new System.Windows.Forms.PictureBox();
+            this.eventName = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonBookNow = new System.Windows.Forms.Button();
+            this.aboutEvent = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PlanovaLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEvent)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -106,11 +113,81 @@
             this.panel2.Size = new System.Drawing.Size(1011, 51);
             this.panel2.TabIndex = 7;
             // 
+            // pictureBoxEvent
+            // 
+            this.pictureBoxEvent.BackColor = System.Drawing.Color.White;
+            this.pictureBoxEvent.Location = new System.Drawing.Point(26, 96);
+            this.pictureBoxEvent.Name = "pictureBoxEvent";
+            this.pictureBoxEvent.Size = new System.Drawing.Size(439, 232);
+            this.pictureBoxEvent.TabIndex = 8;
+            this.pictureBoxEvent.TabStop = false;
+            // 
+            // eventName
+            // 
+            this.eventName.AutoSize = true;
+            this.eventName.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eventName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(17)))), ((int)(((byte)(153)))));
+            this.eventName.Location = new System.Drawing.Point(483, 96);
+            this.eventName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.eventName.Name = "eventName";
+            this.eventName.Size = new System.Drawing.Size(161, 31);
+            this.eventName.TabIndex = 2;
+            this.eventName.Text = "Event Name";
+            this.eventName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(489, 130);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(495, 198);
+            this.flowLayoutPanel1.TabIndex = 9;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(26, 381);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(655, 170);
+            this.flowLayoutPanel2.TabIndex = 10;
+            // 
+            // buttonBookNow
+            // 
+            this.buttonBookNow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonBookNow.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonBookNow.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBookNow.ForeColor = System.Drawing.Color.White;
+            this.buttonBookNow.Location = new System.Drawing.Point(796, 346);
+            this.buttonBookNow.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonBookNow.Name = "buttonBookNow";
+            this.buttonBookNow.Size = new System.Drawing.Size(188, 40);
+            this.buttonBookNow.TabIndex = 11;
+            this.buttonBookNow.Text = "Book Now";
+            this.buttonBookNow.UseVisualStyleBackColor = false;
+            // 
+            // aboutEvent
+            // 
+            this.aboutEvent.AutoSize = true;
+            this.aboutEvent.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutEvent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(17)))), ((int)(((byte)(153)))));
+            this.aboutEvent.Location = new System.Drawing.Point(27, 347);
+            this.aboutEvent.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.aboutEvent.Name = "aboutEvent";
+            this.aboutEvent.Size = new System.Drawing.Size(212, 31);
+            this.aboutEvent.TabIndex = 12;
+            this.aboutEvent.Text = "About the Event";
+            this.aboutEvent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // EventDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
             this.ClientSize = new System.Drawing.Size(1010, 617);
+            this.Controls.Add(this.aboutEvent);
+            this.Controls.Add(this.buttonBookNow);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.eventName);
+            this.Controls.Add(this.pictureBoxEvent);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "EventDetails";
@@ -118,7 +195,9 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PlanovaLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEvent)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -130,5 +209,11 @@
         private System.Windows.Forms.Label PlanovaName;
         private System.Windows.Forms.PictureBox PlanovaLogo;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBoxEvent;
+        private System.Windows.Forms.Label eventName;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button buttonBookNow;
+        private System.Windows.Forms.Label aboutEvent;
     }
 }
