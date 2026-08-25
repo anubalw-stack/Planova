2c08f2018c732c2217ba4fd8b5e21deb8d666c9e
Asanthika Imbulpitiya <AsanthikaI@op.ac.nz>
2026-08-25 12:00:13 +1200

    Asanthika: Sean's Payment was manually merged to Master.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace tech_titans.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -176,6 +176,26 @@ namespace tech_titans.Properties {
         internal static System.Drawing.Bitmap jkisdfgokj {
             get {
                 object obj = ResourceManager.GetObject("jkisdfgokj", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Planova {
+            get {
+                object obj = ResourceManager.GetObject("Planova", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Planova1 {
+            get {
+                object obj = ResourceManager.GetObject("Planova1", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
