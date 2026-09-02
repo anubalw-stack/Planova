7d77330c951bd08188880108c1b758b2f67a10b7
vinodinduwara <induwarvinod@gamil.com>
2026-09-03 10:47:56 +1200

    Vinod: basic design done

---
+++ BookingConfirmationForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tech_titans
+{
+    public partial class BookingConfirmationForm : Form
+    {
+        public BookingConfirmationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BookingConfirmationForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDateTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblLocationTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblQuantityTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPriceTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTotalPaidTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPaymentStatusTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
